d1131656c851175ee2ff1ceda56e00938f962aff
Vildan <vildan84@mail.ru>
2022-07-18 21:37:03 +0300

    Second hard task ready

--- HardExtra/Program.cs
+++ HardExtra/Program.cs
@@ -46,60 +46,78 @@ void PrintArray(int[] col)
     Console.WriteLine();
 }
 
-int[] point1 = CreatePoint(); 
-int[] point2 = CreatePoint(); 
-int[] point3 = CreatePoint();
+// Задача1
+// int[] point1 = CreatePoint(); 
+// int[] point2 = CreatePoint(); 
+// int[] point3 = CreatePoint();
 
-PrintArray(point1); PrintArray(point2); PrintArray(point3);
+// PrintArray(point1); PrintArray(point2); PrintArray(point3);
 
-double dist1 = Math.Sqrt(Math.Pow(point1[0], 2) + Math.Pow(point1[1], 2));
-double dist2 = Math.Sqrt(Math.Pow(point2[0], 2) + Math.Pow(point2[1], 2));
-double dist3 = Math.Sqrt(Math.Pow(point3[0], 2) + Math.Pow(point3[1], 2));
+// double dist1 = Math.Sqrt(Math.Pow(point1[0], 2) + Math.Pow(point1[1], 2));
+// double dist2 = Math.Sqrt(Math.Pow(point2[0], 2) + Math.Pow(point2[1], 2));
+// double dist3 = Math.Sqrt(Math.Pow(point3[0], 2) + Math.Pow(point3[1], 2));
     
-Console.WriteLine($"Dist1:{dist1} Dist2:{dist2} Dist3:{dist3}");
+// Console.WriteLine($"Dist1:{dist1} Dist2:{dist2} Dist3:{dist3}");
 
-int[] firstPoint = point1;
-int[] secondPoint = point2;
-int[] thirdPoint = point3;
-
-
-if (dist2 < dist1 && dist2 < dist3) firstPoint = point2;
-else if (dist3 < dist2) firstPoint = point3;
+// int[] firstPoint = point1;
+// int[] secondPoint = point2;
+// int[] thirdPoint = point3;
 
 
-if (firstPoint == point1)
-{
-    double aa = Math.Sqrt(Math.Pow(point2[0] - point1[0], 2) 
-    + Math.Pow(point2[1] - point1[1], 2));
-    double bb = Math.Sqrt(Math.Pow(point3[0] - point1[0], 2) 
-    + Math.Pow(point3[1] - point1[1], 2));
-    if (aa < bb) secondPoint = point2;
-    else secondPoint = point3;
-}
-else if (firstPoint == point2)
-{
-    double aa = Math.Sqrt(Math.Pow(point1[0] - point2[0], 2) 
-    + Math.Pow(point1[1] - point2[1], 2));
-    double bb = Math.Sqrt(Math.Pow(point3[0] - point2[0], 2) 
-    + Math.Pow(point3[1] - point2[1], 2));
-    if (aa < bb) secondPoint = point1;
-    else secondPoint = point3;
-}
-else if (firstPoint == point3)
-{
-    double aa = Math.Sqrt(Math.Pow(point2[0] - point3[0], 2) 
-    + Math.Pow(point2[1] - point3[1], 2));
-    double bb = Math.Sqrt(Math.Pow(point1[0] - point3[0], 2) 
-    + Math.Pow(point1[1] - point3[1], 2));
-    if (aa < bb) secondPoint = point2;
-    else secondPoint = point1;
-}
+// if (dist2 < dist1 && dist2 < dist3) firstPoint = point2;
+// else if (dist3 < dist2) firstPoint = point3;
+
+
+// if (firstPoint == point1)
+// {
+//     double aa = Math.Sqrt(Math.Pow(point2[0] - point1[0], 2) 
+//     + Math.Pow(point2[1] - point1[1], 2));
+//     double bb = Math.Sqrt(Math.Pow(point3[0] - point1[0], 2) 
+//     + Math.Pow(point3[1] - point1[1], 2));
+//     if (aa < bb) secondPoint = point2;
+//     else secondPoint = point3;
+// }
+// else if (firstPoint == point2)
+// {
+//     double aa = Math.Sqrt(Math.Pow(point1[0] - point2[0], 2) 
+//     + Math.Pow(point1[1] - point2[1], 2));
+//     double bb = Math.Sqrt(Math.Pow(point3[0] - point2[0], 2) 
+//     + Math.Pow(point3[1] - point2[1], 2));
+//     if (aa < bb) secondPoint = point1;
+//     else secondPoint = point3;
+// }
+// else if (firstPoint == point3)
+// {
+//     double aa = Math.Sqrt(Math.Pow(point2[0] - point3[0], 2) 
+//     + Math.Pow(point2[1] - point3[1], 2));
+//     double bb = Math.Sqrt(Math.Pow(point1[0] - point3[0], 2) 
+//     + Math.Pow(point1[1] - point3[1], 2));
+//     if (aa < bb) secondPoint = point2;
+//     else secondPoint = point1;
+// }
  
-if (point1 != firstPoint && point1 != secondPoint) thirdPoint = point1;
-else if (point2 != firstPoint && point2 != secondPoint) thirdPoint = point2;
-else if (point3 != firstPoint && point3 != secondPoint) thirdPoint = point3;
+// if (point1 != firstPoint && point1 != secondPoint) thirdPoint = point1;
+// else if (point2 != firstPoint && point2 != secondPoint) thirdPoint = point2;
+// else if (point3 != firstPoint && point3 != secondPoint) thirdPoint = point3;
 
-PrintArray(firstPoint); PrintArray(secondPoint); PrintArray(thirdPoint); 
+// PrintArray(firstPoint); PrintArray(secondPoint); PrintArray(thirdPoint); 
+
+
+//Задача2
+int[] a = CreatePoint();
+int[] b = CreatePoint();
+int[] c = CreatePoint();
+int[] d = CreatePoint();
+
+PrintArray(a); PrintArray(b); PrintArray(c); PrintArray(d);
+
+double v1 = (c[0] - d[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (c[1] - d[1]);
+double v2 = (c[0] - d[0]) * (c[1] - b[1]) - (c[0] - b[0]) * (c[1] - d[1]);
+double v3 = (b[0] - a[0]) * (b[1] - d[1]) - (b[0] - d[0]) * (b[1] - a[1]);
+double v4 = (b[0] - a[0]) * (b[1] - c[1]) - (b[0] - c[0]) * (b[1] - a[1]);
+
+if (v1 * v2 < 0 && v3 * v4 < 0) Console.WriteLine("Отрезки пересекаются");
+else Console.WriteLine("Отрезки не пересекаются");
 
 
 
